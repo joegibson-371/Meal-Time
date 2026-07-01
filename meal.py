c389001f6c7704b7726b42bbdadba792958839f2
joegibson-371 <joegibson371@gmail.com>
2026-07-01 10:30:36 +0100

    added 12 hour clock functionality

--- meal.py
+++ meal.py
@@ -1,6 +1,6 @@
 def main():
     time=input("What time is it?  ")
-    hours=convert(time)
+    hours=am_pm_convert(time)
     match hours:
         case hours if 7<= hours <=8:
           print("breakfast time")
@@ -10,16 +10,25 @@ def main():
           print("dinner time")
         
           
-
- 
-
-    
-
-
+#Define the function that converts a string typed in the form xx:xx or x:xx
+#into a float wich represents the number of hours.
 def convert(time):
     T=time.strip().split(":")
     hours=int(T[0])+float(int(T[1])/60)
     return(hours)
+
+# here we define another function to convert from 12 to 24 hour time to allow users 
+#to input xx:xx am or xx:xx pm
+def am_pm_convert(time):
+   time=time.strip()
+   if time[-2]=="a":
+      time=time[:-2]
+      return convert(time)
+   elif time[-2]=="p":
+      time=time[:-2]
+      return (convert(time) +12)
+   else:
+      return (convert(time))
 
 
 
